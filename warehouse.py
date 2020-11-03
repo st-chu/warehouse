400067f21e519b3716781e7204f450797025be13
Marek Starzyk <stachu.ms9@gmail.com>
2020-11-03 23:54:13 +0100

    Add items

--- warehouse.py
+++ warehouse.py
@@ -1,6 +1,16 @@
+import tabulate
+
+
+items = [
+    {'name': 'coffee', 'quantity': 10, 'unit': 'pkg.', 'unit_price': 65.99},
+    {'name': 'soy sauce', 'quantity': 7, 'unit': 'btl.', 'unit_price': 14.45},
+    {'name': 'sugar', 'quantity': 50, 'unit': 'kg', 'unit_price': 9.5}
+         ]
+
+
 def menu():
-    print('MENU'.center(80, '-'))
-    print('|{:<78}|'.format(' exit'))
+    print('MENU'.center(80, '_'))
+    print('|{:<78}|'.format('exit'))
     print(''.center(80, '-'))
 
 
@@ -9,4 +19,5 @@ if __name__ == '__main__':
         menu()
         operation_name = input("What would you like to do?: ")
         if operation_name.lower() == 'exit':
+            print('see you later alligator...')
             break
